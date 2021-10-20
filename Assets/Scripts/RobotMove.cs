fc5a1225209429c2ce2d054bdd53f329dbd964d7
mikepmerritt <mikepmerritt@gmail.com>
2021-10-20 02:48:11 -0400

    added basic robot movement inside the arena

---
+++ Assets/Scripts/RobotMove.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMove : MonoBehaviour
+{
+    public float PlayerSpeed;
+    public float MaxX, MaxZ;
+
+    void Update()
+    {
+        // Player movement
+        float forwardMovement = 0f;
+        float strafeMovement = 0f;
+
+        // Check to keep robot in back and front of arena
+        if(Mathf.Abs(transform.position.z) < MaxZ)
+        {
+            forwardMovement = Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime;
+        }
+        else
+        {
+            float inputMovement = Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime;
+            if(Mathf.Abs(transform.position.z + inputMovement) < Mathf.Abs(transform.position.z))
+            {
+                forwardMovement = inputMovement;
+            }
+        }
+
+        // Check to keep robot in left and right of arena
+        if (Mathf.Abs(transform.position.x) < MaxX)
+        {
+            strafeMovement = Input.GetAxis("Horizontal") * PlayerSpeed * Time.deltaTime;
+        }
+        else
+        {
+            float inputMovement = Input.GetAxis("Horizontal") * PlayerSpeed * Time.deltaTime;
+            if (Mathf.Abs(transform.position.x + inputMovement) < Mathf.Abs(transform.position.x))
+            {
+                strafeMovement = inputMovement;
+            }
+        }
+
+        Vector3 totalMovement = (transform.forward * forwardMovement) + (transform.right * strafeMovement);
+        transform.position += totalMovement;
+    }
+}
